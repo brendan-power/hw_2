125ec67cee21b1459dfd38a659f8f737bf8959fa
brendan power <brendan.power@kellogg.northwestern.edu>
2023-05-04 20:58:11 +0000

    mostly done entry, about to fix studio actor error

--- kmdb.rb
+++ kmdb.rb
@@ -79,18 +79,145 @@
 # TODO!
 
 # Prints a header for the movies output
-puts "Movies"
-puts "======"
-puts ""
+# puts "Movies"
+# puts "======"
+# puts ""
 
 # Query the movies data and loop through the results to display the movies output.
 # TODO!
 
 # Prints a header for the cast output
-puts ""
-puts "Top Cast"
-puts "========"
-puts ""
+# puts ""
+# puts "Top Cast"
+# puts "========"
+# puts ""
 
 # Query the cast data and loop through the results to display the cast output for each movie.
 # TODO!
+
+# ASSIGNMENT STARTING SPOT
+# Deletes all data so it doesn't repeat
+Studio.destroy_all
+Movie.destroy_all
+Actor.destroy_all
+Role.destroy_all
+
+# NOTE on naming: When creating objects, name them with an underscore seprating the type of thing and instance
+# NOTE on naming: When descrbing objects for the purpose of creating foreign keys, do not use the underscore
+
+# Adds in data to the studios tables
+
+WarnerBros = Studio.new
+WarnerBros["name"] = "WarnerBros"
+WarnerBros.save
+
+# Adds in data to the movie tables, after naming studios for foreign keys 
+
+studio1 = Studio.find_by({ "name" => "WarnerBros" })
+
+movie_1 = Movie.new
+movie_1["title"] = "Batman Begins"
+movie_1["year_released"] = "2005"
+movie_1["rating"] = "PG-13"
+movie_1["studio_id"] = studio1["id"]
+movie_1.save
+
+movie_2 = Movie.new
+movie_2["title"] = "The Dark Knight"
+movie_2["year_released"] = "2008"
+movie_2["rating"] = "PG-13"
+movie_2["studio_id"] = studio1["id"]
+movie_2.save
+
+movie_3 = Movie.new
+movie_3["title"] = "The Dark Knight Rises"
+movie_3["year_released"] = "2012"
+movie_3["rating"] = "PG-13"
+movie_3["studio_id"] = studio1["id"]
+movie_3.save
+
+
+# Adds in data to the actor tables, after naming the movies for foreign keys 
+movie1 = Movie.find_by({ "title" => "Batman Begins" })
+movie2 = Movie.find_by({ "title" => "The Dark Knight" })
+movie3 = Movie.find_by({ "title" => "The Dark Knight Rises" })
+
+actor_1 = Actor.new
+actor_1["name"] = "Christian Bale"
+actor_1.save
+
+actor_2 = Actor.new
+actor_2["name"] = "Michael Caine"
+actor_2.save
+
+actor_3 = Actor.new
+actor_3["name"] = "Liam Neeson"
+actor_3.save
+
+actor_4 = Actor.new
+actor_4["name"] = "Katie Holmes"
+actor_4.save
+
+actor_5 = Actor.new
+actor_5["name"] = "Gary Oldman"
+actor_5.save
+
+actor_6 = Actor.new
+actor_6["name"] = "Heath Ledger"
+actor_6.save
+
+actor_7 = Actor.new
+actor_7["name"] = "Aaron Eckhart"
+actor_7.save
+
+actor_8 = Actor.new
+actor_8["name"] = "Maggie Gyllenhaal"
+actor_8.save
+
+actor_9 = Actor.new
+actor_9["name"] = "Tom Hardy"
+actor_9.save
+
+actor_10 = Actor.new
+actor_10["name"] = "Joseph Gordon-Levitt"
+actor_10.save
+
+actor_11 = Actor.new
+actor_11["name"] = "Anne Hathaway"
+actor_11.save
+
+
+# Adds in data to the role tables, after naming actors for foreign keys 
+actor1 = Actor.find_by({ "name" => "Christian Bale" })
+actor2 = Actor.find_by({ "name" => "Michael Caine" })
+actor3 = Actor.find_by({ "name" => "Liam Neeson" })
+actor4 = Actor.find_by({ "name" => "Katie Holmes" })
+actor5 = Actor.find_by({ "name" => "Gary Oldman" })
+actor6 = Actor.find_by({ "name" => "Heath Ledger" })
+actor7 = Actor.find_by({ "name" => "Aaron Eckhart" })
+actor8 = Actor.find_by({ "name" => "Maggie Gyllenhaal" })
+actor9 = Actor.find_by({ "name" => "Tom Hardy" })
+actor10 = Actor.find_by({ "name" => "Joseph Gordon-Levitt" })
+actor11 = Actor.find_by({ "name" => "Anne Hathaway" })
+
+
+role_1 = Role.new
+role_1["character_name"] = "Bruce Wayne"
+role_1["movie_id"] = 
+role_1["studio_id"] = 
+role_1.save
+
+
+
+# Data sanity checks
+puts "There are #{Studio.all.count} studios"
+puts WarnerBros.inspect
+puts movie_1.inspect
+puts movie_2.inspect
+puts movie_3.inspect
+
+puts Actor.inspect
+puts actor_1.inspect
+puts actor_9.inspect
+
+
